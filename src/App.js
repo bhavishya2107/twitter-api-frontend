06e856988a6edba4302b05463c78d59febf29f55
Bhavishya Negi <bhavishya2107@gmail.com>
2020-06-20 00:00:35 +0530

    changes in feed component, empty input shows alert

--- src/App.js
+++ src/App.js
@@ -21,73 +21,73 @@ class App extends Component {
   };
 
   userTweets = () => {
-    this.setState({
-      isLoading: true,
-    });
-    const user = {
-      username: this.state.searchInput,
-    };
-    axios
-      .post("https://twitter-api-2107.herokuapp.com/api/v1", user)
-      .then((res) =>
-        this.setState({
-          tweets: res.data.data.statuses,
-          isLoading: false,
-        })
-      );
-    this.state.searchInput = "";
+    if (this.state.searchInput == null) {
+      alert("Invalid Input");
+    } else {
+      this.setState({
+        isLoading: true,
+      });
+      const user = {
+        username: this.state.searchInput,
+      };
+      axios
+        .post("https://twitter-api-2107.herokuapp.com/api/v1", user)
+        .then((res) =>
+          this.setState({
+            tweets: res.data.data.statuses,
+            isLoading: false,
+          })
+        );
+      this.state.searchInput = "";
+    }
   };
 
   render() {
-    console.log(this.state);
     return (
       <>
-      <div className="container-fluid bg">
-        <div className="text-center align-middle">
-          <div className="d-flex align-items-center justify-content-center">
-            <h1>TWEET</h1>
-            <img src={twitter} style={{ width: "100px", height: "100px" }} />
-            <h1>SEARCH</h1>
-          </div>
-          <div class="input-container container">
+        <div className="container-fluid bg">
+          <div className="text-center align-middle">
+            <div className="d-flex align-items-center justify-content-center">
+              <h1>TWEET</h1>
+              <img src={twitter} style={{ width: "100px", height: "100px" }} />
+              <h1>SEARCH</h1>
+            </div>
+            <div class="input-container container">
               <SearchIcon />
-            <input
-              value={this.state.searchInput}
-              onChange={this.onChange}
-              name="searchInput"
-              type="text"
-              class=" input-bar"
-              placeholder="Enter username or #tag"
-            />
+              <input
+                value={this.state.searchInput}
+                onChange={this.onChange}
+                name="searchInput"
+                type="text"
+                class=" input-bar"
+                placeholder="Enter username or #tag"
+              />
+            </div>
+            <button
+              className="btn btn-lg"
+              style={{ background: "#178DE6", color: "white" }}
+              onClick={this.userTweets}
+            >
+              Search
+            </button>
           </div>
-          <button
-            className="btn btn-lg"
-            style={{ background: "#178DE6", color: "white" }}
-            onClick={this.userTweets}
-          >
-            Search
-          </button>
-        </div>
-        <div className="">
-          <div className="row mt-5">
-            {!this.state.isLoading ? (
-              this.state.tweets &&
-              this.state.tweets.map((tweet) => {
-                return (
-                  <div className="col-sm-4 mb-5">
-                    <Feed data={tweet} />
-                  </div>
-                );
-              })
-            ) : (
-              <Spinner />
-            )}
-               {/* <div className="col-sm-4 mb-5">
-                    <Feed />
-                  </div> */}
+          <div className="">
+            <div className="row mt-5">
+              {!this.state.isLoading ? (
+                this.state.tweets &&
+                this.state.tweets.map((tweet) => {
+                  return (
+                    <div className="col-sm-4 mb-5">
+                      <Feed data={tweet} />
+                    </div>
+                  );
+                })
+              ) : (
+                <Spinner />
+              )}
+            </div>
           </div>
         </div>
-      </div>
       </>
     );
   }
--- src/components/feed.js
+++ src/components/feed.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
 import "../stylesheets/feed.css";
-import twitter from "../../src/twitter.svg";
 import heart from "../heart.svg";
 import retweet from "../retweet.svg";
 import comment from "../comment.svg";
@@ -10,35 +9,46 @@ class Feed extends Component {
   constructor() {
     super();
   }
+
+  splitDate(data) {
+    let c = data.split(" ")
+    let newDate = c[0] + " " + c[1] + " "+ c[2] + " " + c[5]
+    return newDate
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   render() {
     return (
       <div>
         <div className=" feed">
           <div className="d-flex bd-highlight mb-3">
             <img
-              src={twitter}
+              src={this.props.data.user.profile_image_url}
               className="profile-image p-2 bd-highlight"
               height="75px"
               width="75px"
             />
             <div className="p-2 bd-highlight">
-              <p className="twitter-name">Bhavishya Negi</p>
-              <p className="twitter-handle">@bhavishya2107</p>
+              <p className="twitter-name">{this.props.data.user.name}</p>
+              <p className="twitter-handle">{"@" + this.props.data.user.screen_name}</p>
             </div>
 
-            <span className="ml-auto p-2 bd-highlight">22/06/20</span>
+            <span className="ml-auto p-2 bd-highlight">{this.splitDate(this.props.data.created_at)}</span>
           </div>
           <p className="card-text tweet-text">{this.props.data.text}</p>
 
           <div className="d-flex justify-content-around">
             <span>
-              <img src={heart} style={{ height: "20px" }} /> {"2.8k"}
+              <img src={heart} style={{ height: "20px" }} /> {this.props.data.favorite_count}
             </span>
             <span>
-              <img src={retweet} style={{ height: "20px" }} /> {"4.3k"}
+              <img src={retweet} style={{ height: "20px" }} /> {this.props.data.retweet_count}
             </span>
             <span>
-              <img src={comment} style={{ height: "20px" }} /> {"11.5k"}
+              <img src={comment} style={{ height: "20px" }} /> {this.getRandomInt(100)}
             </span>
           </div>
         </div>
